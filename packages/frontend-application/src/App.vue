4d9eba80ee26e9e5476ba0abfb66e6e0632fc4a9
terasurfer <me@terasurfer.com>
2019-09-06 20:50:29 -0700

    feat: Login page

---
+++ packages/frontend-application/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <navbar />
+    <transition name="fade">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/common/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/scss/app.scss';
+html, body {
+  height: 100%;
+}
+#app {
+    height: 100%;
+}
+</style>
---
+++ packages/frontend-application/src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-container class="d-flex justify-content-center h-100 align-items-center">
+        <b-card class="border-0 shadow login-card" header-bg-variant="primary">
+            <template v-slot:header>
+                <h5 class="mb-0 text-white text-center">SSO - User Login</h5>
+            </template>
+            <b-form>
+                <b-form-group 
+                    label="Email address:">
+                    <b-form-input type="email" required placeholder="Enter Email"></b-form-input>
+                </b-form-group>
+                <b-form-group 
+                    label="Password:">
+                    <b-form-input type="password" required placeholder="Enter Password"></b-form-input>
+                </b-form-group>
+                <div class="text-center">
+                    <b-btn variant="outline-primary" size="sm" class="btn-block">Sign In</b-btn>
+                    <b-btn variant="primary" size="sm" class="btn-block">Sign In with facebook</b-btn>
+                    <b-btn variant="danger" size="sm" class="btn-block">Sign In with Google</b-btn>
+                </div>
+            </b-form>
+        </b-card>
+    </b-container>
+</template>
+
+<style lang="scss" scoped>
+    .login-card{
+        width: 350px;
+    }
+</style>
